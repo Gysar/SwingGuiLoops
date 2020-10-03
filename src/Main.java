b645d26f28bd5c4889df7badb975ffbc0fefe745
Gysar <gysar.lp@gmail.com>
2020-10-03 21:17:50 +0200

    beispiel angepasst.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		MyTaskExample mtc = new MyTaskExample();
-		Gui gui = new Gui(mtc, 99, 99);
+		Gui gui = new Gui(mtc, 20, 20);
 		gui.start(); // <- gui ruft mtc.generate(this)
 	}
 
--- src/MyTaskExample.java
+++ src/MyTaskExample.java
@@ -11,22 +11,13 @@ public class MyTaskExample {
 	void generate(Gui gui, int rows, int columns) {
 
 //			malt eine Pyramide
-     		gui.setWaitMs(0);
+     		gui.setWaitMs(100);
 			int n = gui.getWidth();
 			for (int i = 0; i < (n / 2) + 1; i++) {
-				for (int j = 0; j < 2 * i + 1 + n / 2 - i; j++) {
+				for (int j = 0; j < 2 * i  +n%2 + n / 2 - i; j++) {
 					if(!(j < n / 2 - i))gui.rectangleAt(j, i, Color.GREEN);
 				}
 			}
 
-//		for(int i=0;i<gui.getWidth();i++) {
-//			for(int j=0;j<gui.getHeight();j++) {
-//				gui.rectangleAt(i, j, Color.RED);
-//			}
-//		}
-			
-//		int[] i={0,1,2,3,4};
-//		gui.rectangleAt(i, i, Color.RED);
-			
 	}
 }
